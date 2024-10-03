3ae9a3200aafd2a286df063922fb4ff844578a86
Osvaldo28th <L24550306@chihuahua2.tecnm.mx>
2024-10-03 16:17:08 -0600

    Add files via upload

---
+++ EVA2_1_EVALUACION.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_evaluacion;
+
+/**
+ *
+ * @author Osvaldo Chavez
+ */
+public class EVA2_1_EVALUACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //REPETICION DE INSTRUCIONES
+        //CICLOS O BUCLES
+        {double val;
+    }
+        double val;
+        
+        
+        // FOR (desde) cuando saben cuantas veces se van a repetir las instruccines
+        // Es un contador
+        
+        //WHILE (Minetras) cuando se repite cero o N veces
+        //DO WHILE (hacer mientras)cuando se repite al menos 1 vezhasta veces n sea condicionales
+        // Ciclo FOR
+        // i == indice
+        System.out.println("");
+        for (int i = 1; i <= 10; i ++){//La variable i solo existe dentro del FOR
+            System.out.print(i + " - ");
+        }
+        System.out.println("");
+        for (int i = 10; i >= 1; i --){
+            System.out.print(i + " - ");
+            
+            
+            
+            // ++ = de uno en uno
+            // -- = resta una unidad
+            // += Suma N valor
+            // -= Resta N valor
+            // *= multiplica por N valor
+            // /= Divide entre N valor
+            // Alcance --> El lugar donde existe un identificador 
+            //Bloque de codigo {}
+        }
+    }
+    
+}
